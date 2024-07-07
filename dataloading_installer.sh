491e8b4d36155e6412763af7201d8f3a15b08132
kai3581 <bodhikailen@gmail.com>
2024-07-06 23:24:09 -0700

    Update dataloading_installer.sh

--- dataloading_installer.sh
+++ dataloading_installer.sh
@@ -7,9 +7,9 @@ chmod +x cleardata.sh
 mkdir -p data_dir/train #create data directory
 mkdir -p data_dir/test
 mkdir -p data_dir/validate
-mkdir etc
+mkdir lib
 touch data_dir/.data_content data_dir/.data_content_shuf
 mkdir bin #isolate executables
 mv dataload dataalloc loaddata.sh cleardata.sh bin
 echo "export PATH="/gscratch/uwb/NETID/dataloading/bin:$PATH"" >> ~/.bashrc #add to PATH
-mv dataload.c dataalloc.c dataloading_installer.sh README.md etc
+mv dataload.c dataalloc.c dataloading_installer.sh lib
