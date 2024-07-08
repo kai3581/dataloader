9681e4c4bc915d80c5b0e23b05c67c35c2f9a497
kai3581 <bodhikailen@gmail.com>
2024-07-08 10:26:42 -0700

    Update cleardata.sh
    removed unnecessary steps

--- cleardata.sh
+++ cleardata.sh
@@ -2,5 +2,4 @@
 dataDir=/gscratch/uwb/NETID/dataloader/dataDir
 
 rm $dataDir/* 2> /dev/null
-rm $dataDir/.data_content $dataDir/.data_content_shuf
 touch $dataDir/.data_content $dataDir/.data_content_shuf
