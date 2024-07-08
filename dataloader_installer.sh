3af284261ea2eb5a212e957fd70085a0301993b4
kai3581 <bodhikailen@gmail.com>
2024-07-08 13:40:34 -0700

    Update dataloader_installer.sh

--- dataloader_installer.sh
+++ dataloader_installer.sh
@@ -4,11 +4,13 @@ gcc -o dataload dataload.c
 gcc -o dataalloc dataalloc.c
 chmod +x loaddata.sh
 chmod +x cleardata.sh
+chmod +x allocdata.sh
 mkdir dataDir
 mkdir lib #isolate code and reference
 mkdir bin #isolate executables
 touch dataDir/.data_content dataDir/.data_content_shuf
-mv dataload dataalloc loaddata.sh cleardata.sh bin
+mv dataload dataalloc loaddata.sh cleardata.sh allocdata.sh bin
 echo "alias loaddata='/gscratch/uwb/NETID/dataloader/bin/loaddata.sh'" >> ~/.bashrc
 echo "alias cleardata='/gscratch/uwb/NETID/dataloader/bin/cleardata.sh'" >> ~/.bashrc
+echo "alias allocdata='/gscratch/uwb/NETID/dataloader/bin/allocdata.sh'" >> ~/.bashrc
 mv dataload.c dataalloc.c dataloader_installer.sh lib
